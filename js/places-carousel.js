3cfe4f8b4ff1e2b51fe816777dba426a4c5988a4
MrsMelnychenko <melnychenko.mariana@gmail.com>
2022-08-30 15:15:19 +0000

    started adding carousel to places block

--- js/places-carousel.js
+++ js/places-carousel.js
@@ -1,28 +1,36 @@
-(function(){
-  const placesSlides [
+(function () {
+  const placesSlides = [
     `<div class="places__slide">
 
         <div class="carousel-places">
             <img class="places-carousel-image" src="img/places/national_history_museum.png" alt="national_history_museum">
-            <h2 class="places-carousel-title">The Natural History Museum</h2>
-            <p class="places-carousel-text">The Natural History Museum text.</p>
-            <a class="read-more-places" href="https://yp34v.weblium.site/">Read more →</a>
         </div>
 
         <div class="carousel-places">
           <img class="places-carousel-image" src="img/places/buckinham_palace.png" alt="buckinham_palace">
-            <h2 class="places-carousel-title">Buckingham Palace</h2>
-            <p class="places-carousel-text">Buckingham Palace text.</p>
-            <a class="read-more-places" href="https://yp34v.weblium.site/">Read more →</a>
           </div>
 
         <div class="carousel-item">
           <img class="places-carousel-image" src="img/places/kew_garden.png" alt="kew_garden">
-          <h2 class="places-carousel-title">Kew Gardens</h2>
-          <p class="places-carousel-text">Kew Gardens text.</p>
-          <a class="read-more-places" href="https://yp34v.weblium.site/">Read more →</a>
         </div>
 
     </div>`
   ];
+
+  let currentPlacesSlideIdx = 0;
+
+  function renderPlacesCarousel() {
+    const placesCarouselCont = document.querySelector('places__slide');
+    placesCarouselCont.innerHTML = placesSlides[currentPlacesSlideIdx];
+  }
+
+  function nextPlace() {
+    currentPlacesSlideIdx = currentPlacesSlideIdx + 1 >= placesSlides.length ? 0 : currentPlacesSlideIdx + 1;
+    renderPlacesCarousel();
+  }
+
+  setInterval(nextPlace, 2000);
+
+  renderPlacesCarousel();
+
 })();
