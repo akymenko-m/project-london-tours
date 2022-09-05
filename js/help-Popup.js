898b1a63a80fef2d30eb2be1ad35c52daa13a23b
MrsMelnychenko <melnychenko.mariana@gmail.com>
2022-09-05 13:03:21 +0000

    Still dealing with modal help popup

---
+++ js/help-Popup.js
@@ -0,0 +1,16 @@
+(function () {
+    const helpPopupWindow = document.querySelector(".book__tour-popup-window");
+    const openPopupBtn = document.querySelectorAll(".open-popup");
+    const closeBtnPopup = document.querySelector(".close-button-popup");
+  
+    function openPopup(el) {
+      tourPopupWindow.classList.toggle("show-modal");
+      el.stopPropagation();
+    }
+  
+    openPopupBtn.forEach((btns) => {
+      btns.addEventListener("click", openPopup);
+    });
+  
+    closeBtnPopup.addEventListener("click", openPopup);
+  })();
